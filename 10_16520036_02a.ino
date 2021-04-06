4c87a1ec5a84fa0994ee6047081feaf84d1dfaeb
hanafathiyah <hanafathiyah9b21@gmail.com>
2021-04-06 17:08:13 +0700

    Revisi file 10_16520036_02a.ino memperbaiki kesalahan penulisan program

--- 10_16520036_02a.ino
+++ 10_16520036_02a.ino
@@ -9,13 +9,13 @@ void loop()
 {
   digitalWrite(9, HIGH);
   delay(1000); // Wait for 1000 millisecond(s)
-  digitalWrite(9, HIGH);
+  digitalWrite(9, LOW);
   digitalWrite(10, HIGH);
   delay(1000); // Wait for 1000 millisecond(s)
-  digitalWrite(10, HIGH);
+  digitalWrite(10, LOW);
   digitalWrite(11, HIGH);
   delay(1000); // Wait for 1000 millisecond(s)
-  digitalWrite(11, HIGH);
+  digitalWrite(11, LOW);
   digitalWrite(9, HIGH);
   delay(1000); // Wait for 1000 millisecond(s)
 }
